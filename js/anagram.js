25515a59b97604cbe4484bf4b90914b089e4d630
francobko <franco.condor@urp.edu.pe>
2022-06-15 15:02:02 -0500

    anagram

---
+++ js/anagram.js
@@ -0,0 +1,8 @@
+let isAnagram = (str1, str2) => {
+    let sorted1 = str1.split('').sort().join('').toLowerCase();
+    let sorted2 = str2.split('').sort().join('').toLowerCase();
+
+    return sorted1 === sorted2;
+}
+
+console.log(isAnagram('abc','bca'));
